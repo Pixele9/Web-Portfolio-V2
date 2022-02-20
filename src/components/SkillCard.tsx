21b619501cd05b802492faa37bb131c4e7de18c1
Andrés Leal <pixele9@gmail.com>
2022-02-20 00:07:02 -0600

    feat: fetch skills from firestore and component for skill

---
+++ src/components/SkillCard.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+interface Props {
+	title: string,
+    src: string,
+}
+
+export const SkillCard:React.FC<Props> = (props) => {
+  
+  return (
+    <div className="flex flex-col w-40 mb-2 h-40">
+        {/* <div className="bg-white mt-3 mr-3 h-32 flex flex-col justify-between"> */}
+        <div className="w-full h-full">
+            <div className="bg-white w-full h-3/4 flex justify-center items-center">
+                <img className='object-contain w-full h-24 p-4' src={props.src} alt="programming lang"/>
+            </div>
+            <div className='flex w-full h-1/4 px-2 items-center'>
+                <p className='text-white font-bold'>{ props.title }</p>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default SkillCard
---
+++ src/views/Skills.tsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+
+import { db } from "../firebaseDB";
+
+import { collection, query, onSnapshot, orderBy, where } from "firebase/firestore";
+
+import SkillCard from "../components/SkillCard";
+
+export default function Skills() {
+	const [skills, setSkills] = useState([]);
+	const [loading, setLoading] = useState(false);
+
+	useEffect(() => {
+		const getExperience = async () => {
+			setLoading(true);
+			
+			const q = query(
+				collection(db, "skills")
+			)
+
+			onSnapshot(q, (snapShot) => {
+				let skillsArr: Object[] = [];
+				snapShot.forEach((doc) => {
+					const data = doc.data();
+					skillsArr.push(data);
+					console.log("DATA: ", data);
+				});
+				setSkills(skillsArr);
+			});
+			setLoading(false);
+		};
+		getExperience();
+	}, []);
+
+	return (
+		<div className="mt-12">
+			<h1 className="text-4xl font-bold mt-3">SKILLS</h1>
+
+			<h4 className="mt-7 text-purple-400 font-light text-sm">
+				EXPERIENCED WITH
+			</h4>
+			<div className="h-1 w-10 bg-purple-400 mt-2 mb-4"></div>
+
+			<div className="flex flex-wrap justify-between">
+				{ skills.map(el => (
+					<SkillCard key={el.title + Math.random()} title={el.title} src={el.src} />
+				))}
+			</div>
+		</div>
+	);
+}
