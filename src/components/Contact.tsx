f8dc42c2323df11f254dd8470332f43b1a566a69
Andrés Leal <pixele9@gmail.com>
2022-02-20 00:06:42 -0600

    feat: contact button added

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,6 +1,6 @@
 export default function ContactBtn() {
 	return (
-		<button className="rounded-none bg-gray-100 text-sm text-black p-3 w-1/3 mt-7 hover:shadow-xl focus-within:shadow-xl shadow-sm">
+		<button className="rounded-none bg-gray-100 border text-sm text-black p-3 w-1/3 mt-7 hover:shadow-xl focus-within:shadow-xl shadow-sm transform duration-150 active:translate-y-1">
 			Contact Me
 		</button>
 	)
